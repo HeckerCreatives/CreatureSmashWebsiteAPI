586e6e5d91b54271efdb94969fba046767a5db35
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-03-12 18:31:31 +0800

    poshy posh

--- initialization/serverinitialize.js
+++ initialization/serverinitialize.js
@@ -3,13 +3,17 @@ const Users = require("../models/Users")
 const Userwallets = require("../models/Userwallets")
 const Userdetails = require("../models/Userdetails")
 const Score = require("../models/Score")
+const Maintenance = require("../models/Maintenance")
 
 exports.initialize = async (req, res) => {
 
     //  INITIALIZE CREATURE SMASH USER
     const csadmin = await Users.findOne({username: "creaturesmash"})
     .then(data => data)
-    .catch(err => res.status(400).json({ message: "bad-request", data: err.message }))
+    .catch(err => {
+        console.log(`There's a problem getting cs user data ${err}`)
+        return
+    })
 
     if (!csadmin){
         const player = await Users.create({_id: new mongoose.Types.ObjectId("65eeeeabd9576a4f8ae38afd"), username: "creaturesmash", password: "e7FNO0EWgW11", gametoken: "", webtoken: "", bandate: "none", banreason: "", status: "active"})
@@ -51,6 +55,27 @@ exports.initialize = async (req, res) => {
 
                 console.log(`Server Initialization Failed, Error: ${err}`);
     
+                return
+            })
+        })
+    }
+
+    const maintenancelist = await Maintenance.find()
+    .then(data => data)
+    .catch(err => {
+        console.log("there's a problem getting maintenance list")
+
+        return
+    })
+
+    if (maintenancelist.length <= 0){
+        const maintenancelistdata = ["fightgame", "eventgame", "fullgame"]
+
+        maintenancelistdata.forEach(async maintenancedata => {
+            await Maintenance.create({type: maintenancedata, value: "0"})
+            .catch(err => {
+                console.log(`there's a problem creating maintenance list ${err}`)
+
                 return
             })
         })
