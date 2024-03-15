df3004d51a05c2a81f51711d757a9efe9d38d9df
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-03-15 16:40:58 +0800

    posh

--- controllers/auth.js
+++ controllers/auth.js
@@ -126,52 +126,50 @@ exports.authlogin = async(req, res) => {
                 }
 
                 res.cookie('sessionToken', jwtoken, { secure: true, sameSite: 'None' } )
-                return res.json({message: "success"})
-            })
-            .catch(err => res.status(400).json({ message: "bad-request2", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details."  + err }))
-        }
-        else{
-            return res.json({message: "nouser", data: "Username/Password does not match! Please try again using the correct credentials!"})
-        }
-    })
-    .catch(err => res.status(400).json({ message: "bad-request1", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details." }))
-}
-
-exports.staffauthlogin = async(req, res) => {
-    const { username, password } = req.query;
-
-    Staffusers.findOne({ username: { $regex: new RegExp('^' + username + '$', 'i') } })
-    .then(async user => {
-        if (user && (await user.matchPassword(password))){
-            if (user.status != "active"){
-                return res.status(401).json({ message: 'failed', data: `Your account had been ${user.status}! Please contact support for more details.` });
-            }
-
-            const token = await encrypt(privateKey)
-
-            await Staffusers.findByIdAndUpdate({_id: user._id}, {$set: {webtoken: token}}, { new: true })
-            .then(async () => {
-                const payload = { id: user._id, username: user.username, status: user.status, token: token, auth: user.auth }
-
-                let jwtoken = ""
-
-                try {
-                    jwtoken = await jsonwebtokenPromisified.sign(payload, privateKey, { algorithm: 'RS256' });
-                } catch (error) {
-                    console.error('Error signing token:', error.message);
-                    return res.status(500).json({ error: 'Internal Server Error', data: "There's a problem signing in! Please contact customer support for more details! Error 004" });
-                }
-
-                res.cookie('sessionToken', jwtoken, { secure: true, sameSite: 'None' } )
                 return res.json({message: "success", data: {
-                        auth: user.auth
-                    }
-                })
+                    auth: "player"
+                }})
             })
             .catch(err => res.status(400).json({ message: "bad-request2", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details."  + err }))
         }
         else{
-            return res.json({message: "nouser", data: "Username/Password does not match! Please try again using the correct credentials!"})
+
+            await Staffusers.findOne({ username: { $regex: new RegExp('^' + username + '$', 'i') } })
+            .then(async staffuser => {
+                
+                if (staffuser && (await staffuser.matchPassword(password))){
+                    if (staffuser.status != "active"){
+                        return res.status(401).json({ message: 'failed', data: `Your account had been ${staffuser.status}! Please contact support for more details.` });
+                    }
+
+                    const token = await encrypt(privateKey)
+
+                    await Staffusers.findByIdAndUpdate({_id: staffuser._id}, {$set: {webtoken: token}}, { new: true })
+                    .then(async () => {
+                        const payload = { id: staffuser._id, username: staffuser.username, status: staffuser.status, token: token, auth: staffuser.auth }
+
+                        let jwtoken = ""
+
+                        try {
+                            jwtoken = await jsonwebtokenPromisified.sign(payload, privateKey, { algorithm: 'RS256' });
+                        } catch (error) {
+                            console.error('Error signing token:', error.message);
+                            return res.status(500).json({ error: 'Internal Server Error', data: "There's a problem signing in! Please contact customer support for more details! Error 004" });
+                        }
+
+                        res.cookie('sessionToken', jwtoken, { secure: true, sameSite: 'None' } )
+                        return res.json({message: "success", data: {
+                                auth: staffuser.auth
+                            }
+                        })
+                    })
+                    .catch(err => res.status(400).json({ message: "bad-request2", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details."  + err }))
+                }
+                else{
+                    return res.json({message: "nouser", data: "Username/Password does not match! Please try again using the correct credentials!"})
+                }
+            })
+            .catch(err => res.status(400).json({ message: "bad-request1", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details." }))
         }
     })
     .catch(err => res.status(400).json({ message: "bad-request1", data: "There's a problem with your account! There's a problem with your account! Please contact customer support for more details." }))
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 const router = require("express").Router()
-const { authlogin, staffauthlogin, register, registerstaffs, getreferralusername, logout } = require("../controllers/auth")
+const { authlogin, register, registerstaffs, getreferralusername, logout } = require("../controllers/auth")
 const { protectsuperadmin } = require("../middleware/middleware")
 
 router
     .get("/login", authlogin)
-    .get("/staffauthlogin", staffauthlogin)
     .get("/getreferralusername", getreferralusername)
     .post("/register", register)
     .post("/registerstaffs", protectsuperadmin, registerstaffs)
